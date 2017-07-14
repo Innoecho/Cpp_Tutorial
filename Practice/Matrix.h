f274f8b37e01dfd92173b0052f24cae24de4f87c
Innoecho <Innoecho@outlook.com>
2017-07-14 08:49:21 +0800

    add MatrixTest()

--- Practice/Matrix.h
+++ Practice/Matrix.h
@@ -336,3 +336,35 @@ Matrix<T> Matrix<T>::eye(int n)
 	}
 	return I;
 }
+
+int MatrixTest()
+{
+	double num[3][3] = {{1, 2, 3},
+						{4, 5, 7},
+						{7, 8, 9}};
+
+	Matrix<double> A(num);
+	Matrix<double> B = A.transpose();
+	Matrix<double> C = Matrix<double>::eye(3);
+
+	std::cout << "A = " << std::endl;
+	std::cout << A << std::endl;
+	std::cout << "-A = " << std::endl;
+	std::cout << -A << std::endl;
+	std::cout << "B = " << std::endl;
+	std::cout << B << std::endl;
+	std::cout << "C = " << std::endl;
+	std::cout << C << std::endl;
+	std::cout << "A + B = " << std::endl;
+	std::cout << A + B << std::endl;
+	std::cout << "A - B = " << std::endl;
+	std::cout << A - B << std::endl;
+	std::cout << "2 * A = " << std::endl;
+	std::cout << 2.0 * A << std::endl;
+	std::cout << "A / 2 = " << std::endl;
+	std::cout << A / 2 << std::endl;
+	std::cout << "A * B = " << std::endl;
+	std::cout << A * B << std::endl;
+
+	return 0;
+}
